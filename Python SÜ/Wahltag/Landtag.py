51b2aff9bd383942d0165b6671c7ae6c657ece88
RonaldMcDonald <maxkoch@tsn.at>
2023-01-30 23:32:29 +0100

    warum gehts ned? idk man

--- Python SÜ/Wahltag/Landtag.py
+++ Python SÜ/Wahltag/Landtag.py
@@ -1,5 +1,5 @@
 '''
-Datenanalyse Landtagswahl 2018 in Imst
+Datenanalyse Landtagswahlen 1989, 1994, 1999, 2003, 2008, 2013, 2018 in Wörgl
 Autor: Maximilian Koch
 Datum: 24.10.2021
 '''
@@ -7,36 +7,38 @@ Datum: 24.10.2021
 # Importieren der Module
 import matplotlib.pyplot as plt
 
-# Importieren der Daten
-def import_woergl(file):
-    file = open("2018.csv", "r")  #file wird geöffnet
-    woergl = []   #liste wird erstellt
+def woergl(j):
+    file = open(f"{j}.csv" , "r")
+    woergl = []
 
-    for line in file:   #Wenn "Imst" gefunden wurde, splitten
+    for line in file:
         if "Wörgl" in line:
             woergl = line.split(";")
-    file.close()    #file wieder schließen
-    return woergl     #Wert wieder zurückgeben
+    file.close()
 
-#Importieren der Wahlbeteiligung
-def import_wahlbeteiligung (woergl):
-    print(woergl)
-    for i in range(0, 10,1):    #Alle Dokumentationen bis "Wahlbeteiligung" werden gelöscht
+    for a in range (0 , 10 , 1):
         woergl.pop(0)
-    print(woergl)
-    wahlbeteiligung = woergl[0].replace(";", ".") #Strichpunkte werden zu Punkte
-    return float(wahlbeteiligung)  #Zurückgeben des Wertes
 
-wahljahre = [1989, 1994, 1999, 2003, 2008, 2013, 2018]  #Diese Variablen werden zum erstellen des Diagramms benötigt
+    woergl = str(woergl)
+    woergl = woergl.replace("," , ".")
+
+
+    wahlbeteiligung = woergl[2:7]
+
+    return wahlbeteiligung
+
+
+jahre = [1989 , 1994 , 1999 , 2003 , 2008 , 2013 , 2018]
 wahlbeteiligung = []
 
-for i in wahljahre:
-    # fügt die wahlbeteiligung des jahres in die liste ein
-    wahlbeteiligung.append(import_wahlbeteiligung(import_woergl("{}.csv".format(i))))
+for j in jahre:
+    wahlbeteiligung.append(woergl(j))
 
-
-#Erstellen des Diagramms
-plt.title("Wahlbeteiligung in Imst", fontsize=20) # Titel
-plt.ylim(0, 100) # Y-Achse von 0 bis 100
-plt.plot(jahre, wahlbeteiligung, label="Wahlbeteiligung") # Daten
+#print (wahlbeteiligung)
+plt.title("Wahlbeteiligung in Wörgl", fontsize=20) # Titel
+plt.ylim(0 , 50) # Y-Achse von 0 bis 100
+plt.plot(jahre , wahlbeteiligung, label="Wahlbeteiligung") # Daten
 plt.show() # Diagramm anzeigen
+
+
+#print(woergl())
