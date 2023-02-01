cfb4fcaf636dc54b0e22992f57dcb8d5878ca812
RonaldMcDonald <maxkoch@tsn.at>
2023-02-01 13:41:59 +0100

    dei mama

--- Shooting Range/Continue.py
+++
@@ -1,6 +0,0 @@
-names = ["Kaspar", "Umut", "Jeremias", "Elisabeth", "Bürsa"]
-
-if "Jeremias" not in names:
-    print("E")
-else:
-    continue
--- Shooting Range/names_in_python.py
+++
@@ -1,8 +0,0 @@
-names = ["Kaspar", "Umut", "Jeremias", "Elisabeth", "Bürsa"]
-f = open("a.txt", "w")
-for name in names:
-    if "a" in name:
-        f.write(name)
-        f.write(", ")
-
-f.close()
